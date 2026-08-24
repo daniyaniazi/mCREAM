9753a5c9f931df4f49d20e6ede08f10cd9c10230
Daniya Niazi <farihadania@hotmail.com>
2026-08-24 23:26:03 +0200

    add: fix src error

--- scripts/export_test_image_ids.py
+++ scripts/export_test_image_ids.py
@@ -1,7 +1,11 @@
 import argparse
+import sys
 from pathlib import Path
 
 import pytorch_lightning as pl
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(PROJECT_ROOT))
 
 from src.utils import get_component_with_dicts, load_config
 
